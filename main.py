d0abc04f35cff071b1367c95de7be9e0b2d399ba
keren <kerenr@gmail.com>
2019-07-21 08:34:14 +0300

    browse and search functionality

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 from bottle import (get, post, redirect, request, route, run, static_file,
                     template)
-import utils
+import json                    
+from utils import getVersion, getShowsData, search
 
 # Static Routes
 
@@ -20,19 +21,23 @@ def img(filepath):
 @route('/')
 def index():
     sectionTemplate = "./templates/home.tpl"
-    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData = {})
+    return template("./pages/index.html", version=getVersion(), sectionTemplate=sectionTemplate, sectionData = {})
 
 @route('/browse')
 def browse():
     sectionTemplate = "./templates/browse.tpl"
-    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData = {})
+    return template("./pages/index.html", version=getVersion(), sectionTemplate=sectionTemplate, sectionData = getShowsData())
 
 @route('/search')
-def browse():
+def search():
     sectionTemplate = "./templates/search.tpl"
-    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData = {})
+    return template("./pages/index.html", version=getVersion(), sectionTemplate=sectionTemplate, sectionData = {})
 
-
+@post('/search')
+def search():
+    sectionTemplate = "./templates/seaerch_result.tpl"
+    query = request.forms.get('q')
+    return template("./pages/index.html", version=getVersion(), sectionData={}, sectionTemplate=sectionTemplate, query=query, results=search(query))
 
 if __name__ == "__main__":
     run(host='localhost', port=os.environ.get('PORT', 5000), reloader=True, debuger=True)
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from bottle import template
 import json
+from operator import itemgetter
 
 JSON_FOLDER = './data'
 AVAILABE_SHOWS = ["7", "66", "73", "82", "112", "143", "175", "216", "1371", "1871","2993", "305"]
@@ -12,3 +13,22 @@ def getJsonFromFile(showName):
         return template("{folder}/{filename}.json".format(folder=JSON_FOLDER, filename=showName))
     except:
         return "{}"
+
+def getShowsData():
+    shows = []
+    for show in AVAILABE_SHOWS:
+        shows.append(json.loads(getJsonFromFile(show)))
+    return sorted(shows, key=itemgetter('name'))
+
+def search(search):
+    shows = getShowsData()
+    list = []
+    for show in shows:
+        for episode in show['_embedded']['episodes']:
+            if str(episode['name']).lower().find(search).lower()) > -1:
+                element = {
+                    'showid': show['id'],
+                 'episodeid': episode['id'],
+                  'text': '{}: {}'.format('show['name'], episode['name'])}
+                list.append(element)
+    return list
